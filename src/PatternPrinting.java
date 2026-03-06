1785ddd16802bb148e39bff7bb2f06c2b8d29e16
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-03-06 20:56:06 +0530

    Completed pattern printing experiment

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i<n; i++) {
+            System.out.print("* ");}
+        for (int j = 1; j<=n+1; j++) {
+                System.out.println("* ");
+        }
+       
+        sc.close();
+       
+    }
 }
