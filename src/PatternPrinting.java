2955195c94c4efbab5edae3714dd67b2a712173f
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-03-08 10:17:24 +0530

    Update pattern printing logic per README

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 public class PatternPrinting {
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        for (int i = 1; i<n; i++) {
-            System.out.print("* ");}
-        for (int j = 1; j<=n+1; j++) {
-                System.out.println("* ");
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                if (i < n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.println("*");
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.println("*");
+            }
         }
         sc.close();
     }
